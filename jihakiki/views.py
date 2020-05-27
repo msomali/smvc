262eac8c4e93efc53373f4ab4f7d91a98e3218f5
msomali <waleed92@live.com>
2020-05-27 22:22:49 +0300

    Added SMS folder and populated views.py and urls.py for Github Webhook testing

--- jihakiki/views.py
+++ jihakiki/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+# Test Github Webhook
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
+
+# Test Github Webhook
+@csrf_exempt
+def hello(request):
+    return HttpResponse('pong')
--- smvc/urls.py
+++ smvc/urls.py
@@ -18,7 +18,11 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+# Test Github Webhook
+from jihakiki import  views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('hello/', views.hello, name='hello'),
 ]
