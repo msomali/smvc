c3aabd2dc5d25a5e8492e6a5a0318093f27cd728
msomali <waleed92@live.com>
2020-05-28 00:35:36 +0300

    Replaced . with jihakiki in urls.py

--- smvc/urls.py
+++ smvc/urls.py
@@ -19,7 +19,7 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 # Test Github Webhook
-from . import views
+from jihakiki import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
