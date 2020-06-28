00fd188acc07b69e4f8d3ae0a58bb4f80cf54a87
msomali <waleed92@live.com>
2020-06-28 20:45:35 +0300

    added service options to registered mjumbe and review section

--- jihakiki/views.py
+++ jihakiki/views.py
@@ -422,28 +422,33 @@ def webhook(request):
             # Review Service
             elif qry_mjumbe.verification_status=="Verified" and qry_mjumbe.is_active=="Yes" and keyword[0].upper()=="HAKIKI":
                 # Check PIN
-                keyword_pin = int(keyword[2])
-                if keyword_pin==qry_mjumbe.pin:
-                    qry_mwananchi = Mwananchi.objects.get(id__exact=keyword[1])
-                    # Check for mwananchi
-                    if qry_mwananchi:
-                        # Check Mwananchi Active and Verification status
-                        if qry_mwananchi.is_active=="Yes" and qry_mwananchi.verification_status=="Unverified":
-                            qry_mwananchi.step += 1
-                            qry_mwananchi.mjumbe_id = qry_mjumbe.id
-                            qry_mwananchi.save()
-                        else:
-                            return HttpResponse(json.dumps({
-                                'messages': [
-                                    {'content': "Samahani, namba ya usajili ya mwananchi imesitishwa au imeshahakikiwa."}
-                                ]
-                            }), 'application/json')
-                    else:
-                        return HttpResponse(json.dumps({
-                            'messages': [
-                                {'content': "Samahani, namba ya usajili ya mwananchi haipo."}
-                            ]
-                        }), 'application/json')
+                # keyword_pin = int(keyword[2])
+                if int(keyword[2])==qry_mjumbe.pin:
+                    return HttpResponse(json.dumps({
+                        'messages': [
+                            {'content': "PIN: "+int(keyword[2])}
+                        ]
+                    }), 'application/json')
+                #     qry_mwananchi = Mwananchi.objects.get(id__exact=keyword[1])
+                #     # Check for mwananchi
+                #     if qry_mwananchi:
+                #         # Check Mwananchi Active and Verification status
+                #         if qry_mwananchi.is_active=="Yes" and qry_mwananchi.verification_status=="Unverified":
+                #             qry_mwananchi.step += 1
+                #             qry_mwananchi.mjumbe_id = qry_mjumbe.id
+                #             qry_mwananchi.save()
+                #         else:
+                #             return HttpResponse(json.dumps({
+                #                 'messages': [
+                #                     {'content': "Samahani, namba ya usajili ya mwananchi imesitishwa au imeshahakikiwa."}
+                #                 ]
+                #             }), 'application/json')
+                #     else:
+                #         return HttpResponse(json.dumps({
+                #             'messages': [
+                #                 {'content': "Samahani, namba ya usajili ya mwananchi haipo."}
+                #             ]
+                #         }), 'application/json')
                 else:
                         return HttpResponse(json.dumps({
                             'messages': [
