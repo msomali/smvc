41579a0bf4592a4380f7e70ee4f893ef9a04c7e4
msomali <waleed92@live.com>
2020-08-05 15:20:46 +0300

    fixed bug for VEO to Mjumbe review

--- jihakiki/views.py
+++ jihakiki/views.py
@@ -1914,7 +1914,7 @@ def webhook(request):
                             ###### Disable Existing PIN Before Generating Another
                             qry_pin = Pin.objects.filter(generator_id__exact=qry_veo.id, project__exact=project, service__exact=service)
                             if qry_pin:
-                                qry_pin = qry_pin.get(generator_id__exact=qry_veo.id, status__excat=status_valid)
+                                qry_pin = qry_pin.get(generator_id__exact=qry_veo.id, status__exact=status_valid)
                                 qry_pin.status = status_invalid
                                 qry_pin.save()
 
